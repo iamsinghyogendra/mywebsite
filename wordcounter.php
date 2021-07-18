ec78e99db5ec3ce10c73e45581eec320eb57227c
Yogendra Singh <61265398+iamsinghyogendra@users.noreply.github.com>
2021-07-18 08:22:35 +0545

    Add files via upload

---
+++ wordcounter.php
@@ -0,0 +1,204 @@
+Government Jobs
+Government occupations are one of the highest-paying job categories in Nepal. They are lavished with praise and admiration. Thousands of individuals fight for government positions every year in the Lok Sewa Aayog's rigorous and difficult tests. Government employment roles are divided into several levels and divisions. If you wish to work for the government directly after finishing your ten plus two, you can apply for a non-gazetted rank. If you wish to pursue an officer position, you must first complete your bachelor's degree. Officer positions are available in both technical and non-technical capacities. Anyone with a BBA or BBS degree can apply for government positions in the banking sector.
+
+Similarly, engineering and information technology students might apply for technical officers in their particular fields of interest. The ministry of finance announced a 20 percent increase in the basic income of non-gazetted government workers and an 18 percent increase in the basic compensation of gazetted civil servants during the release of the budget for the year 2076/2077. Government employees, security personnel, teachers, and employees of state-owned corporations are all eligible for the raise.
+
+IT Proffesional
+IT (Information and Technology) is a fairly common career in Nepal. Within the field of information technology, there are several career opportunities, and it is also one of the highest paid occupations in Nepal. In Nepal, you may study BSC CSIT or computer engineering in colleges/institutions affiliated with several universities. There are presently more than five colleges that offer this degree. From technical officer to web designer, the role might be varied. As a web designer, you would create websites and web apps. Mobile applications, computer software, and other types of software are created by software designers. You must be an expert in the use of computers and coding. You should be experienced with software development, including web apps, Android apps, and so on.
+
+There are many companies who are trying to hire software engineers. A software developer's average pay in Nepal is about one lakh Nepali rupees. And, of course, there will be wage increases over time. IT workers' salaries vary based on their role and the firm they work for. It might cost anything from hundreds to lakhs. The beginning wage for an IT graduate may be as low as 10,000 NPR, but this will increase as you get more experience and develop a more sophisticated set of abilities. People working in Nepal's IT industry typically earn between 16,679 and 76,002 NPR per month (actual maximum salary is higher). The entire monthly pay, including bonuses and perks, is shown below. If you want to pursue IT as a career, you'll need to study science in high school and then enroll in an engineering college.
+
+Engineers
+Engineering is another of Nepal's highest-paying professions. It is a well-known job option around the world. Even in Nepal, the desire to study engineering is only increasing.
+There are several forms of engineering, and civil engineering is seen to be the best option in Nepal. Scholarships are available in institutions that provide engineering courses, however completion is difficult as usual. You can either work for a firm or establish your own consultancy after completing your Bachelor of Engineering. Because Nepal is still a developing country, it requires more civil engineers than the estimated 25 thousand already employed. A civil engineer's compensation might range from $25k to $5 million. Similarly, after completing a four-year college degree and obtaining one year of experience in the area, a specialist in information and innovation may make up to $80,000 per month.
+
+Lawyers & Advocate
+Working in the realm of law, providing legal services, is one of Nepal's highest-paying occupations. Lawyers place such a great importance on their clients' perceptions of them. A skilled lawyer may save you a lot of time and money in the long run. That is why they are handsomely compensated. Judges, too, are frequently tasked with deciding people's fates, which entails a great deal of responsibility. As a result, their large incomes appear to be entirely fair. Attorneys, on the other hand, require a lot of directions, tolerance, and openness. They also disagree and settle their differences on their own. This explains why persons in the legal profession in Nepal make a lot of money.
+
+For attorney review, lawyers acquire, arrange, and examine legal materials. They also do research on norms and laws in order to prepare case files. They also contact with clients and witnesses to organize meetings and interviews, if needed. You can choose between a three-year Diploma in Law (PCL) or a Bachelor's Degree in Law (B.L.) program to become a lawyer. You can then seek a master's degree for further study. Lawyers in Nepal make between 20,000 and 20,000 rupees per year. They easily have one of Nepal's highest-paying occupations.
+
+Surgeon:
+A surgeon is a doctor who does surgery, and becoming one takes extensive training and experience. Surgeons are classified according to their specialities, which influences how much money they make. Surgeoning is a difficult job because other people's lives are dependent on their abilities. A surgeon must have at least eight years of experience and a medical degree. As a result, they enjoy one of Nepal's highest-paying occupations. Cardiac surgeons, dental surgeons, neurologists, colon and rectal surgeons, and other specialties may be found in Nepal. Surgeons, for example, are at the top of the list of high-paying medical occupations since they demand extensive expertise and also involve a significant level of risk owing to the job's crucial nature.
+
+General practitioners, on the other hand, make a fair living. They serve patients with a variety of non-surgical health care services, including as detecting and treating illnesses, as well as other common disorders. Their annual income might range from $30,000 to $80,000. If you work at a larger hospital with a good reputation, your income will improve. 
+Similarly, experts' pay ranges from 80k to 2 million dollars. Medical specialists operate in a variety of settings in the healthcare industry, including prescriptions, dentistry, pharmacy stores, mental health care, birthing help, surgery, and so on.
+
+CEO - Chief Executive Officer
+A company's highest-ranking executive is the Chief Executive Officer (CEO). It is one of Nepal's highest-paying jobs. It's not easy, though, because becoming a CEO requires more than ten years of expertise. Many people aspire to be CEOs, but they seldom discuss the obstacles they face on the job. In a firm, a CEO alone has a lot of obligations to do. They devise plans to keep the firm running smoothly. It is not an exaggeration to state that the CEO has the company's fate in his or her hands, as they decide on the company's path. They are under pressure to carry out their responsibilities effectively while also guiding managers and other executive bodies. CEOs must make prudent judgments and ensure that their company is effectively mobilizing resources. They also serve as a link between general businesses and the board of directors. Similarly, CEOs are in charge of a company's whole operation. They took on the job of board director to assist with important decisions. Their duty is to ensure that they validate the company's culture while developing business plans. In addition, the amount of years it takes to become a CEO varies depending on the company and its size. It should be between 9 and 20 years old, preferably more.
+
+Aviation Professionals 
+The aviation sector is another source of high-paying jobs in Nepal. To work as an aviation professional, you'll need a bachelor's degree, a pilot or air traffic control course, and a year of experience.
+With the growing number of private airlines in Nepal, the prospects are expanding. If you want to make a profession out of aviation, you should first study science before enrolling in aviation/engineering school. 
+Pilots must complete rigorous training programs and have a minimum of 1,000 hours of flying time. Pilots are also accountable for the safety of hundreds of thousands of passengers on a daily basis once they are fit for the job. As a result, aviation is a lot of fun. Aside from that, the pay is excellent. Pilots in Nepal make between 2 and 3 lakhs per month on average.
+
+Bankers
+Bankers work in the banking industry and assist in the raising of capital for the institution. People are also supervised in terms of their finances. In Nepal, bankers are well compensated. In addition, many companies engage bankers to help them with their accounting transactions. Nepalese bankers might earn up to four lakh rupees each month. A month's salary in Nepal for a banking employee is about 87,800 NPR. The lowest average income is 27,600 NPR, while the highest average pay is 180,000 NPR.
+
+Furthermore, bank managers earn between 92,300 and 302,000 NPR each year. Bank managers are in control of billions of dollars in assets ranging from cash to investments.
+Given the essential duties and risk that come with running a bank, it's understandable that bankers have high-paying positions. This is the monthly average wage, which includes housing, transportation, and other benefits. Different banking positions pay considerably different amounts.
+
+
+
+
+
+
+
+
+
+<style>
+
+
+:root {
+    --bg-table-stripe: #f6f6f5;
+    --b-table: #e3e3e2;
+    --caption: #242423;
+}
+
+table {
+    background-color: transparent;
+    border-collapse:collapse;
+  	font-family: Arial, Helvetica, sans-serif
+}
+
+th {
+    text-align:left
+}
+
+.dcf-txt-center {
+      text-align: center!important
+    }
+
+    .dcf-txt-left {
+      text-align: left!important
+    }
+
+    .dcf-txt-right {
+      text-align: right!important
+    }
+    
+.dcf-table caption {
+      color: var(--caption);
+      font-size: 1.13em;
+      font-weight: 700;
+      padding-bottom: .56rem
+    }
+
+    .dcf-table thead {
+      font-size: .84em
+    }
+
+    .dcf-table tbody {
+      border-bottom: 1px solid var(--b-table);
+      border-top: 1px solid var(--b-table);
+      font-size: .84em
+    }
+
+    .dcf-table tfoot {
+      font-size: .84em
+    }
+
+    .dcf-table td, .dcf-table th {
+      padding-right: 1.78em
+    }
+
+    .dcf-table-bordered, .dcf-table-bordered td, .dcf-table-bordered th {
+      border: 1px solid var(--b-table)
+    }
+
+    .dcf-table-bordered td, .dcf-table-bordered th, .dcf-table-striped td, .dcf-table-striped th {
+      padding-left: 1em;
+      padding-right: 1em
+    }
+
+    .dcf-table-bordered tr:not(:last-child), .dcf-table-striped tr:not(:last-child) {
+      border-bottom: 1px solid var(--b-table)
+    }
+
+    .dcf-table-striped tbody tr:nth-of-type(2n) {
+      background-color: var(--bg-table-stripe)
+    }
+
+    .dcf-table thead td, .dcf-table thead th {
+      padding-bottom: .75em;
+      vertical-align: bottom
+    }
+
+    .dcf-table tbody td, .dcf-table tbody th, .dcf-table tfoot td, .dcf-table tfoot th {
+      padding-top: .75em;
+      vertical-align: top
+    }
+
+    .dcf-table tbody td, .dcf-table tbody th {
+      padding-bottom: .75em
+    }
+
+    .dcf-table-bordered thead th {
+      padding-top: 1.33em
+    }
+
+    .dcf-wrapper-table-scroll {
+      overflow-x: auto;
+      -webkit-overflow-scrolling: touch;
+      left: 50%;
+      margin-left: -50vw;
+      margin-right: -50vw;
+      padding-bottom: 1em;
+      position: relative;
+      right: 50%;
+      width: 100vw
+    }
+
+    @media only screen and (max-width:42.09em) {
+      .dcf-table-responsive thead {
+        clip: rect(0 0 0 0);
+        -webkit-clip-path: inset(50%);
+        clip-path: inset(50%);
+        height: 1px;
+        overflow: hidden;
+        position: absolute;
+        width: 1px;
+        white-space: nowrap
+      }
+      .dcf-table-responsive tr {
+        display: block
+      }
+      .dcf-table-responsive td {
+        -webkit-column-gap: 3.16vw;
+        -moz-column-gap: 3.16vw;
+        column-gap: 3.16vw;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        text-align: left!important
+      }
+      .dcf-table-responsive.dcf-table-bordered, .dcf-table-responsive.dcf-table-bordered thead th {
+        border-width: 0
+      }
+      .dcf-table-responsive.dcf-table-bordered tbody td {
+        border-top-width: 0
+      }
+      .dcf-table-responsive:not(.dcf-table-bordered) tbody tr {
+        padding-bottom: .75em
+      }
+      .dcf-table-responsive:not(.dcf-table-bordered) tbody td {
+        padding-bottom: 0
+      }
+      .dcf-table-responsive:not(.dcf-table-bordered):not(.dcf-table-striped) tbody td {
+        padding-right: 0
+      }
+      .dcf-table-responsive.dcf-table-bordered tbody tr:last-child td:last-child {
+        border-bottom-width: 0
+      }
+      .dcf-table-responsive tbody td:before {
+        content: attr(data-label);
+        float: left;
+        font-weight: 700;
+        padding-right: 1.78em
+      }
+    }
+
+.dcf-overflow-x-auto {
+      overflow-x: auto!important;
+      -webkit-overflow-scrolling: touch
+    }
+    
+</style>
